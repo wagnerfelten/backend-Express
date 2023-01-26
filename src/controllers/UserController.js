302fdc6c323d4d0e052493a9d29ef2c325276e42
WagnerRodrigues <felten19@gmail.com>
2023-01-26 17:26:05 -0300

    criado arquivos para erros atraves da api do express-async-errors

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError");
+
 class UseControllers {
   create(request, response) {
     const { name, email, password } = request.body;
+
+    if(!name){
+      throw new AppError("Name is required!!!")
+    }
 
     response.status(201).send(
       `Nome: ${name} <br /> email: ${email} <br /> senha: ${password}`
--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+requeri("express-async-errors");
+
+
 const express = require("express");
 
 const routes = require("./Routes");
@@ -7,6 +10,9 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use((error, request, response, next) => {
+
+});
 
 const PORT = 1919;
 app.listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT ${PORT}`));
