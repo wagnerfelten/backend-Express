747b1433daf9d8da44281b30ecacf31e2bcbefb4
WagnerRodrigues <felten19@gmail.com>
2023-01-26 18:04:01 -0300

    finalizado estudo de Node.js-Explorer

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-requeri("express-async-errors");
-
+require("express-async-errors");
+const AppError = require("./utils/AppError");
 
 const express = require("express");
 
@@ -11,7 +11,19 @@ app.use(express.json());
 app.use(routes);
 
 app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 
+  console.log(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
 });
 
 const PORT = 1919;
